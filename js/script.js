48a2789fdce06de45c43210989db75e694c53cf1
OleksiyTaranenko <77008282+OleksiyTaranenko@users.noreply.github.com>
2023-07-05 14:53:11 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,43 +1,51 @@
-function convertCToF(celsius) {
-    return celsius * 9/5 + 32;
-};
+let temp = "kelvinToFahrenheit";
+let celsius = 44;
+let fahrenheit = 73;
+let kelvin = 285;
 
-let cToF = convertCToF(30);
+switch (temp) {
+    case "celsiusToFahrenheit":
+        console.log(celsiusToFahrenheit(celsius));
+        break;
+    case "celsiusToKelvin":
+        console.log(celsiusToKelvin(celsius));
+        break;
+    case "fahrenheitToCelsius":
+        console.log(fahrenheitToCelsius(fahrenheit));
+        break;
+    case "fahrenheitToKelvin":
+        console.log(fahrenheitToKelvin(fahrenheit));
+        break;
+    case "kelvinToCelsius":
+        console.log(kelvinToCelsius(kelvin));
+        break;
+    case "kelvinToFahrenheit":
+        console.log(kelvinToFahrenheit(kelvin));
+        break;
+    default:
+        break;
+}
 
-function convertCToK(celsius) {
-    return celsius + 273.15;
-};
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9) / 5 + 32;
+}
 
-let cToK = convertCToK(30);
+function celsiusToKelvin(kelvin) {
+    return kelvin + 273.15;
+}
 
-function convertFToC(fahrenheit) {
-    return (fahrenheit - 32) * 5/9;
-};
+function fahrenheitToCelsius(fahrenheit) {
+    return ((fahrenheit - 32) * 5) / 9;
+}
 
-let fToC = convertFToC(86);
+function fahrenheitToKelvin(fahrenheit) {
+    return (fahrenheit - 32) / 1.8 + 273.15;
+}
 
-function convertFToK(fahrenheit) {
-    return (fahrenheit - 32) * 5/9 + 273.15;
-};
-
-let fToK = convertFToK(86);
-
-function convertKToC(kelvin) {
+function kelvinToCelsius(kelvin) {
     return kelvin - 273.15;
-};
+}
 
-let kToC = convertKToC(303.15);
-
-function convertKToF(kelvin) {
-    return (kelvin - 273.15) * 9/5 + 32;
-};
-
-let kToF = convertKToF(303.15);
-
-function convert(degrees, scale) {    
-    if (scale == 'celsius') return console.log(degrees + '°C is ' + cToF + '°F is ' +  cToK + '°K');
-    if (scale == 'fahrenheit') return console.log(degrees + '°F is ' + fToC + '°C is ' + fToK + '°K');
-    if (scale == 'kelvin') return console.log(degrees + '°K is ' + kToC + '°C is ' + kToF + '°F');    
-}    
-    
-convert(86, 'fahrenheit');
+function kelvinToFahrenheit(kelvin) {
+    return (kelvin - 273.15) * 1.8 + 32;
+}
